7cf6e59a9f0454d7f8b68d644d0c96d4486c9bd1
gezahegnw <gezahegnw@gmail.com>
2018-10-31 14:47:17 -0500

    another update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,111 +13,103 @@ var connection = mysql.createConnection({
 
   // Your password
   password: "password",
-  database: "Bamazon"
+  database: "bamazon"
 });
 
-connection.connect(function(err) {
+/*connection.connect(function(err) {
   if (err) throw err;
   showMeAllPoduct();
 });
 
-
+*/
 //=====================================================================
 // Show ids, names, and prices of products
 
-    // Prompt user to select a product and enter desired quantity
-    function productsInfo() {
-      inquirer.prompt( [
-      {
-        name: "itemId",
-        type: "input",
-        message: "Enter the id of the product you would like to buy."
-      }, {
-        name: "quantity",
-        type: "input",
-        message: "How many would you like to purchase?"
-      }
+  
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
 
-   
-     
-    ]).then(function(answer) {
-       var item1 = answer.product;//product;
-     // var item1 = parseFloat(answer.quantity * response[i].price).toFixed(2);
-      //var quantity1= response[i].stock_quantity - answer.quantity;
-      var quantity1 = answer.quantity; 
-       // var quantity1 = answer.quantity; 
-        connection.query("SELECT * FROM products WHERE ?", { item_id: item1},function (err, response) {
-          if (err) throw err;
+// Show ids, names, and prices of products
+connection.query('SELECT * FROM `products`', function (err, results, fields) {
+  if (err) {
+    console.log(err);
+  }
+  for (var i=0; i<results.length; i++) {
+    console.log(chalk.green("Item_Id: ") + results[i].item_id + " " + chalk.blue("Porduct_Name: ") +
+    results[i].product_name + " " + chalk.green("Department: ") +
+    results[i].department_name + " [" + chalk.yellow("Price: ") +
+    results[i].price.toString() + "]" + " " + chalk.red("Quantity: ") +
+    results[i].stock_quantity.toString());  }
+  // Prompt user to select a product and enter desired quantity
+  function saleItems() {
+    inquirer.prompt( [{
+      name: "itemId",
+      type: "input",
+      message: "Enter the id of the product you'd like to buy."
+    }, {
+      name: "quantity",
+      type: "input",
+      message: "How many would you like to purchase?"
+    }]).then(function(answer) {
+      for (var i=0; i<results.length; i++) {
+        if (results[i].item_id === parseInt(answer.itemId)) {
+          // If order quantity is too high, notify user of insufficient stock
+          if (results[i].stock_quantity < parseInt(answer.quantity)) {
+            console.log(chalk.red("Insufficient stock!"));
+            saleItems();
+          } else {
+            // Calculate order total and remaining stock
+            var total = parseFloat(answer.quantity*results[i].price).toFixed(2);
+            var newStock = results[i].stock_quantity - answer.quantity;
 
-          if(response.length === 0){
-            console.log("Error: please select a valid Item ID from the products list.");
-            showMeAllPoduct();
+            // Construct query to update stock
+            var updateStock = 'UPDATE `products` SET `stock_quantity` = ' + newStock + ' WHERE `item_id` = ' + answer.itemId
+            connection.query(updateStock, function(err, result) {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log(chalk.blue(result.affectedRows + " product updated"));
+                keepShopping();
+              }
+            });
+
+            // Notify user of successful purchase
+            console.log("You have purchased " + answer.quantity + " " + results[i].product_name);
+            console.log("Your order total is " + total);
+          //  keepShopping();
           }
-          else{
-            var productInStock = response[0];
-            if(quantity1 <= productInStock.stock_quantity){
-              console.log("Good news! We have the porduct is in stock....you can place your order!");
-            
-          var updateTheInventory = "UPDATE products SET stock_quantity = " + (productInStock.stock_quantity - quantity1) + "WHERE item_id = " + item1;
-          connection.query(updateTheInventory, function(err, data){
-            if(err) throw err;
-            console.log("Your order has been placed successfuly! Your total would be $" + productInStock.price * quantity1);
-            console.log("Thank you for shopping with us.");
-            console.log("====================================");
-            keepShopping();
-          })
-        } else {
-          console.log("Sorry, item's not in stock to place your order this time.\n" + "please change your order.\n" + "your item was " +
-          productInStock.product_name + " and it has " + productInStock.stock_quantity + " left in stock.");
-          keepShopping();
         }
       }
-    })
-  
-    })
-  
+    });
   }
-  
+    saleItems();
 
-function showMeAllPoduct() {
-    connection.query("SELECT * FROM products", function(err, results) {
-      if (err) {
-        console.log(err);
+  function keepShopping() {
+    inquirer.prompt([
+      {
+        name: "confirm",
+        type: "confirm",
+        message: "Would you like to keep shopping?",
       }
-      for (var i=0; i < results.length; i++) {
-        console.log(chalk.green("Item_Id: ") + results[i].item_id + " " + chalk.blue("Porduct_Name: ") +
-          results[i].product_name + " " + chalk.green("Department: ") +
-          results[i].department_name + " [" + chalk.yellow("Price: ") +
-          results[i].price.toString() + "]" + " " + chalk.red("Quantity: ") +
-          results[i].stock_quantity.toString());
-      
-      } 
+    ]).then(function(response){
+      if(response.confirm){
+        console.log("================================================");
+        saleItems();
+      }else{
+        console.log("Thank you for shopping with Bamazon");
+        connection.end();
+      };
      
-      productsInfo();
-   });
+    });
+  };
+  //saleItems();
+});
 
-  
-  }
-
-function keepShopping() {
-  inquirer.prompt([
-    {
-      type: "fonfirm",
-      message: "Would you like to keep shopping?",
-      name: "confirm"
-    }
-  ]).then(function(response){
-    if(response.confirm){
-      console.log("================================================");
-      showMeAllPoduct();
-    }else{
-      console.log("Thank you for shopping with Bamazon");
-      connection.end();
-    };
-   
-  });
-};
-    
     
 
       
